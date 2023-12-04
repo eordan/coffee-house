51677361f1a8016b3acb5ed15566797fcd0050d9
eordan <pistsouandrey@gmail.com>
2023-12-04 21:38:56 +0300

    feat: install and configure sass

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import sass from 'sass'
+
+
+export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass,
+      },
+    },
+  },
+});
