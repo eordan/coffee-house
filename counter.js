2e535acd89f77c021a6a967774cedf2e2c35a134
eordan <pistsouandrey@gmail.com>
2023-12-04 22:09:25 +0300

    fix: eslint errors

--- counter.js
+++ counter.js
@@ -1,9 +1,11 @@
-export function setupCounter(element) {
-  let counter = 0
+export default function setupCounter(element) {
+  let counter = 0;
+  const counterElement = element;
   const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+    counter = count;
+
+    counterElement.innerHTML = `count is ${counter}`;
+  };
+  counterElement.addEventListener('click', () => setCounter(counter + 1));
+  setCounter(0);
 }
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
 import sass from 'sass';
 
-
 export default defineConfig({
   plugins: [eslint()],
   css: {
