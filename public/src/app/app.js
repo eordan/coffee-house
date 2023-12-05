1e02f691647f1c2890cd62a64851c13665779f8c
eordan <pistsouandrey@gmail.com>
2023-12-05 21:23:43 +0300

    feat: add variables.scss (fill in with project colors)

---
+++ public/src/app/app.js
@@ -0,0 +1,8 @@
+import './app.scss';
+// import addMenu from '../components/menu/menu.js';
+// import addMainPage from '../pages/main/main.js';
+// import addContact from '../components/contact/contact.js';
+
+// addMenu();
+// addMainPage();
+// addContact();
