4be6857de8603f1fc08e330e0321f7e1873a3720
eordan <pistsouandrey@gmail.com>
2023-12-07 23:12:16 +0300

    refactor: rename menu block into footer

--- src/app/app.js
+++ src/app/app.js
@@ -1,8 +1,8 @@
 import './app.scss';
-import addMenu from '../pages/menu/menu.js';
+import addHeader from '../components/header/header.js';
 import addMainPage from '../pages/main/main.js';
-import addContact from '../components/footer/footer.js';
+import addFooter from '../components/footer/footer.js';
 
-addMenu();
+addHeader();
 addMainPage();
-addContact();
+addFooter();
