6a5b0ca796b1fd5cfec6d406e6e2750216c06067
eordan <pistsouandrey@gmail.com>
2023-12-08 21:13:44 +0300

    feat: switch on enjoy and choose components on the main page

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,11 +1,11 @@
-// import addEnjoy from '../../components/enjoy/enjoy.js';
-// import addChoose from '../../components/choose/choose.js';
+import addEnjoy from '../../components/main-enjoy/main-enjoy.js';
+import addChoose from '../../components/main-choose/main-choose.js';
 // import addResource from '../../components/resource/resource.js';
 // import addDownload from '../../components/download/download.js';
 
 export default function addMainPage() {
-  // addEnjoy();
-  // addChoose();
+  addEnjoy();
+  addChoose();
   // addResource();
   // addDownload();
 }
