526187f8463ef7d4711f83a15c2babbc5d62f791
eordan <pistsouandrey@gmail.com>
2023-12-12 21:41:58 +0300

    feat: add resource component on main page

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,11 +1,11 @@
 import addEnjoy from '../../components/main-enjoy/main-enjoy.js';
 import addChoose from '../../components/main-choose/main-choose.js';
-// import addResource from '../../components/resource/resource.js';
+import addResource from '../../components/main-resource/main-resource.js';
 // import addDownload from '../../components/download/download.js';
 
 export default function addMainPage() {
   addEnjoy();
   addChoose();
-  // addResource();
+  addResource();
   // addDownload();
 }
